9e888f987b983f5c348cb7494e371bef8fcd369e
Ismaël Cherfi <cherfi.ismael@gmail.com>
2022-06-01 13:53:23 +0200

    Remplacement des h5 du carrousel par des h3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
-let navbarElement = document.querySelector(".navbar");
+let navbarElement = document.querySelector('.navbar');
 
-
-window.addEventListener("scroll",()=>{
-      let scrollTop = window.scrollY
-      console.log(scrollTop);
-    if (scrollTop > 100){
-        navbarElement.classList.remove("hidden")
-    } else if (scrollTop < 100) {
-        navbarElement.classList.add("hidden")
-    }
-
-
-
-
-})
+window.addEventListener('scroll', () => {
+	let scrollTop = window.scrollY;
+	console.log(scrollTop);
+	if (scrollTop > 100) {
+		navbarElement.classList.remove('hidden');
+	} else if (scrollTop < 100) {
+		navbarElement.classList.add('hidden');
+	}
+});
