632abef8d094046adc8c85115912a56d5ff2c720
Ismaël Cherfi <cherfi.ismael@gmail.com>
2022-06-01 12:00:01 +0200

    Navbar et flêche animé, header 98%

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let navbarElement = document.querySelector(".navbar");
+
+
+window.addEventListener("scroll",()=>{
+      let scrollTop = window.scrollY
+      console.log(scrollTop);
+    if (scrollTop > 100){
+        navbarElement.classList.remove("hidden")
+    } else if (scrollTop < 100) {
+        navbarElement.classList.add("hidden")
+    }
+
+
+
+
+})
